ea3798a41615e82c214b571b86518e0f47b2879d
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-04-29 16:28:57 -0700

    Bug fix: the usb interface name is not fix, we need to figure it out each time

--- server_setup_scripts/r440_network.sh
+++ server_setup_scripts/r440_network.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 config_file=/etc/netplan/01-network-manager-all.yaml
-interface_names="eno2 enx00249b46c78b"
+
+interface_names="eno2"
+
+# Look for the USB-Ethernet interface which name is not fixed.
+usb_interface_name=$(ip addr | grep -Po '^[0-9][0-9]*:.*\Kenx[^:][^:]*')
+if [ "${usb_interface_name}" ]; then
+	# If found, add it to the list of interfaces to configure
+    interface_names="${interface_names} ${usb_interface_name}"
+else
+    echo "WARINIG: USB-Ethernet device not found! It will not be configured!"
+fi
 
 # Run network configuration checks
 . network_configration_checks.sh
@@ -12,7 +22,7 @@ echo "    eno2:
       dhcp6: no
       mtu: 9000
       addresses: [10.0.1.1/21, ]
-    enx00249b46c78b:
+    ${usb_interface_name}:
       dhcp4: no
       dhcp6: no
       addresses: [192.168.1.1/24,]" >> ${config_file}
