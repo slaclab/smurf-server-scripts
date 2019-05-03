fc87115fedea2d6a1dc39dd8435a513a47465100
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-05-03 16:25:09 -0700

    Add empty lines

--- server_setup_scripts/server_type.sh
+++ server_setup_scripts/server_type.sh
@@ -4,10 +4,12 @@ echo "Detecting type of server..."
 if dmidecode | grep -Fq R440
 then
     echo "This is as Dell R440 server"
+    echo
     dell_r440=1
 elif dmidecode | grep -Fq R330
 then
     echo "This is a Dell R330 server"
+    echo
     dell_r330=1
 else
     echo "Unsupported server"
