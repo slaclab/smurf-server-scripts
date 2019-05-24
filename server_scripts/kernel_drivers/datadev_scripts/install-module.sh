df65c1975dfaa2f2620c04492427f98530dfa4c1
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-05-24 16:49:40 -0700

    Bug fix: add missing version folder

--- server_scripts/kernel_drivers/datadev_scripts/install-module.sh
+++ server_scripts/kernel_drivers/datadev_scripts/install-module.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ -z "$(lsmod | grep datadev)" ]; then
-    insmod /usr/local/src/datadev/datadev.ko
+    insmod /usr/local/src/datadev/%%VERSION%%/datadev.ko
     sysctl -w vm.max_map_count=262144
 fi
