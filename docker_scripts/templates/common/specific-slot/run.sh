ebcc723f82c55792af2b872d00398f25e60d0038
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-02-06 10:02:08 -0800

    Add the same input options parser method to the slot-specific run.sh script, so that the users have the same interface respect to the non slot-specific version.

--- docker_scripts/templates/common/specific-slot/run.sh
+++ docker_scripts/templates/common/specific-slot/run.sh
@@ -1,11 +1,54 @@
 #!/usr/bin/env bash
 
+########################
+# Function definitions #
+########################
+
+# Usage message
+usage()
+{
+    echo "Start a docker container"
+    echo ""
+    echo "usage: ${script_name} [-e|--extra-opts <server_extra_opts>]"
+    echo "    -e|--extra-opts   <server_extra_opts> : Extra options to be passed to the pysmurf-server startup script."
+    echo "                                            If passing a option with arguments, or multiple option, wrapped"
+    echo "                                            then with quotes. For example: -e \"--hard-boot -a 10.0.1.102\"."
+    echo "    -h|--help                             : Show this message."
+    echo ""
+    exit 1
+}
+
+#############
+# Main body #
+#############
+
+# Verify inputs arguments
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case ${key} in
+    -e|--extra-opts)
+    extra_opts="\"$2\""
+    shift
+    ;;
+    -h|--help)
+    usage
+    ;;
+esac
+shift
+done
+
+echo
+
+# Stop previously running server, if any by calling stop.sh
 ./stop.sh
 
+# Start the sever
 echo "Starting docker containers..."
 
 if [ -c /dev/datadev_0 ]; then
-    extra_opts="\"$@\"" docker-compose -f docker-compose.yml -f docker-compose.pcie.yml up -d
+    extra_opts=${extra_opts} docker-compose -f docker-compose.yml -f docker-compose.pcie.yml up -d
 else
     docker-compose up -d
 fi
