8d6d89bbaf15222e0f2ad343203416a6c2cc217f
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-02-28 15:46:05 -0800

    Add function to verify if a tag exist in a remote repository

--- docker_scripts/common.sh
+++ docker_scripts/common.sh
@@ -8,3 +8,16 @@ print_git_tags()
 {
     git ls-remote --tags --refs $1 | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | sort -g -t. -r
 }
+
+# Verify if a tag exist in a remote repository
+# The first argument is the repository url,
+# The second argument is the tag
+verify_git_tag_exist()
+{
+	local repo=$1
+	local tag=$2
+
+	git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep ${tag}
+
+	echo $?
+}
