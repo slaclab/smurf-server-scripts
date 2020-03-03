f9fb445a2ae90cd204fb77c8e17b04d75ba76cb9
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-03-03 09:07:35 -0800

    Change tabs to spaces

--- docker_scripts/common.sh
+++ docker_scripts/common.sh
@@ -7,13 +7,13 @@
 # The second argument is optional, and defines string to be excluded (for example 'v1.' to exclude all v1.* versions)
 print_git_tags()
 {
-	local repo=$1
-	local exclude=$2
+    local repo=$1
+    local exclude=$2
 
-	if [ ${exclude} ]; then
-		git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep -v ${exclude} | sort --version-sort
-	else
-    	git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | sort --version-sort
+    if [ ${exclude} ]; then
+        git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep -v ${exclude} | sort --version-sort
+    else
+        git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | sort --version-sort
     fi
 }
 
@@ -23,15 +23,15 @@ print_git_tags()
 # The third argument is optional, and defines string to be excluded (for example 'v1.' to exclude all v1.* versions)
 verify_git_tag_exist()
 {
-	local repo=$1
-	local tag=$2
-	local exclude=$3
+    local repo=$1
+    local tag=$2
+    local exclude=$3
 
-	if [ ${exclude} ]; then
-		local macth=$(git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep -v ${exclude} | grep -P ^${tag}$)
-	else
-		local macth=$(git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep -P ^${tag}$)
-	fi
+    if [ ${exclude} ]; then
+        local macth=$(git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep -v ${exclude} | grep -P ^${tag}$)
+    else
+        local macth=$(git ls-remote --tags --refs ${repo} | sed -E 's/^[[:xdigit:]]+[[:space:]]+refs\/tags\/(.+)/\1/g' | grep -P ^${tag}$)
+    fi
 
-	echo ${macth}
+    echo ${macth}
 }
