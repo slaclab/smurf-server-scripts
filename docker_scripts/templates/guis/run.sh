33705b5749a92f4bf36e8a4c5b378132ce039299
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-04-13 16:58:57 -0700

    Fix typos in template file

--- docker_scripts/templates/guis/run.sh
+++ docker_scripts/templates/guis/run.sh
@@ -2,12 +2,13 @@
 
 user="cryo"
 
-docker run -dit --rm  \
+docker run -it --rm  \
   --log-opt tag=guis \
   -u $(id -u ${user}):$(id -g ${user}) \
   --net host \
   -e DISPLAY \
   -e location=${PWD} \
-  -v /etc/group:/etc/group:ro \
-  -entrypoint connect_remote_gui.py \
+  -v /home/${user}/.Xauthority:/home/${user}/.Xauthority \
+  -v /home/${user}/.bash_history:/home/${user}/.bash_history \
+  --entrypoint connect_remote_gui.py \
   tidair/smurf-rogue:%%SMURF_ROGUE_VERSION%% $@
