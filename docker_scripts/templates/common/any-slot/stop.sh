cc01c89e9ed71bc63383ec56bfad8a0c49f60ec2
cryo <cryo@smurf-srv11>
2022-01-08 20:14:20 -0800

    Fix

--- docker_scripts/templates/common/any-slot/stop.sh
+++ docker_scripts/templates/common/any-slot/stop.sh
@@ -9,6 +9,7 @@ for d in ${pysmurf_name} ${server_name}; do
         echo "${d} already exists in state '$(docker ps -a -f name=${d} --format {{.Status}})'. Stopping and removing it."
 	run_cmd_with_echo "docker stop ${d}"
         run_cmd_with_echo "docker rm ${d}"
+    fi
 done
 
 echo "Done!"
