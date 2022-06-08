c4f4321fc6762f2c27c1cbce47739853ef1f6ea4
cryo <cryo@smurf-srv11>
2022-06-08 07:20:52 -0700

    Revert stop.sh to R3.10.2

--- docker_scripts/templates/common/stop.sh
+++ docker_scripts/templates/common/stop.sh
@@ -1,13 +1,39 @@
 #!/usr/bin/env bash
 
-. ./functions.sh
+# Call the base script
+. ./base.sh
 
+# Stop and remove any pysmurf and smurf server container
 for d in ${pysmurf_name} ${server_name}; do
-    r=$(docker ps -a -f name=${d} | wc -l)
 
-    if [ ${r} != 1 ]; then
-        echo "${d} is in state '$(docker ps -a -f name=${d} --format {{.Status}})'. Stopping and removing it."
-	run_cmd_with_echo "docker stop ${d}"
-        run_cmd_with_echo "docker rm ${d}"
+    # Stop and remove running container, if any
+    echo "Checking if a ${d} container is running on slot ${slot}..."
+    r1=$(docker ps -f name=${d} -f status=running | wc -l)
+
+    if [ ${r1} != 1 ]; then
+        echo "A container is running. Stopping it..."
+        for c in "docker stop ${d}" "docker rm ${d}"; do
+            run_cmd_with_echo ${c}
+        done
+        echo
+    else
+        echo "No container is running."
+        echo
+
+        # If no container was running, remove any non-running container, if any
+        echo "Check if a docker container is non-running state on slot ${slot}..."
+        r2=$(docker ps -a -f name=${d} | wc -l)
+
+        if [ ${r2} != 1 ]; then
+            echo "A container was found, on state '$(docker ps -a -f name=${d} --format {{.Status}})'. Removing it..."
+            run_cmd_with_echo "docker rm ${d}"
+        else
+            echo "No container was found"
+        fi
+        echo
     fi
+    echo
 done
+
+echo "Done!"
+echo
