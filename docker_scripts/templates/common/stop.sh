66b844d3afb47eb088081bac61fff12964fb5895
cryo <cryo@smurf-srv11>
2022-06-08 07:34:13 -0700

    Fix stop.sh

--- docker_scripts/templates/common/stop.sh
+++ docker_scripts/templates/common/stop.sh
@@ -1,7 +1,39 @@
 #!/usr/bin/env bash
 
 # Call the base script
-. ./base.sh
+. ./functions.sh
+
+. .env
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case ${key} in
+	-N|--slot)
+	    slot="$2"
+	    shift
+	    ;;
+	-h|--help)
+	    usage
+	    ;;
+	*)
+	    args="${args} $key"
+	    ;;
+    esac
+    shift
+done
+
+if [ -z ${slot+x} ]; then
+    usage "Slot number not defined."
+else
+    if ! [ ${slot} -ge 2 -a ${slot} -le 7 ]; then
+        usage "Invalid slot number. Must be a number between 2 and 7."
+    fi
+fi
+
+server_name=smurf_server_s${slot}
+pysmurf_name=pysmurf_s${slot}
 
 # Stop and remove any pysmurf and smurf server container
 for d in ${pysmurf_name} ${server_name}; do
